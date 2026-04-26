aa4f7d0d546268eb783d57eda2b63d498738118c
vanshikajuyal7 <vanshikajuyal0@gmail.com>
2026-04-27 01:47:19 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,19 @@
+# Simple Calculator
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    print("Result:", num1 + num2)
+elif operator == "-":
+    print("Result:", num1 - num2)
+elif operator == "*":
+    print("Result:", num1 * num2)
+elif operator == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Cannot divide by zero")
+else:
+    print("Invalid operator")
